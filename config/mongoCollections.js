550f9e67cdbc98a58bf302fd94522d016036ca5c
Parshotan Seenanan <parshsee@gmail.com>
2023-03-27 15:29:13 -0400

    Created inital files, updated README

---
+++ config/mongoCollections.js
@@ -0,0 +1,18 @@
+import { dbConnection } from './mongoConnection.js';
+
+const getCollectionFn = (collection) => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+// TODO:
+// Add DB Collections
+// export const <<COLLECTION_NAMES>> = getCollectionFn('<<COLLECTION_NAMES>>');
