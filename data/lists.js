6e7dbcdb69840865e3b9501c8bc10a86629eadf5
Parth Panchal <parth.sdis@gmail.com>
2023-04-07 17:11:30 -0400

    added data function descriptions to each data file

---
+++ data/lists.js
@@ -0,0 +1,5 @@
+// create list - used by /list/new route
+// view list - /profile/:username/lists - getAll(), /list/:id - get()
+// edit list - used by /list/:id route
+// delete list - used by /list/:id route
+// delete entry from list - used by /list/:id route
---
+++ data/users.js
@@ -0,0 +1,4 @@
+// create user - used by /signup
+// view user - used by /login, /profile, /profile/:username
+// edit user - used by /profile/:username
+// delete user - used by /profile/:username
