1c3b48a66df1c5a7db419d253b85821d47ab5c46
Parth Panchal <parth.sdis@gmail.com>
2023-04-10 16:29:07 -0400

    completed functions for activity, will integrate trending update functions into them once they're done added some more validation functions too

--- data/activity.js
+++ data/activity.js
@@ -2,3 +2,115 @@
 // view activity - used by profile/username/activity and /activity/:id
 // edit activity - used by /activity/:id
 // delete activity - used by /activity/:id, also call update function in trending.js
+
+import { activity } from "../config/mongoCollections.js";
+import { trending } from "../config/mongoCollections.js";
+import { ObjectId } from "mongodb";
+import * as validation from "../utils/validation.js";
+
+const createLog = async (movieId, username, review, rating, date) => {
+  validation.checkProvided(movieId, username, review, rating);
+  movieId = validation.checkMovieId(movieId);
+  // i guess we don't need to check username here, since we're just using it to link the activity to the user...still keeping it here just in case
+  username = validation.checkUsername(username);
+  // do we need to have a check here to also see if the username exists in the database, or will that be taken care of during the session creation?
+  review = validation.checkString(review, "Review");
+  rating = validation.checkRating(rating, "Rating");
+  date = validation.checkDate(date, "Date");
+
+  let newLog = {
+    movieId: movieId,
+    username: username,
+    review: review,
+    rating: rating,
+    date: date,
+  };
+
+  const logs = await activity();
+  const newLogInfo = await logs.insertOne(newLog);
+
+  if (!newLogInfo.insertedId || !newLogInfo.acknowledged)
+    throw "Error: could not add activity";
+  return await getLogById(newLogInfo.insertedId.toString());
+};
+
+// Get an activity log by its _id
+const getLogById = async (activityId) => {
+  activityId = validation.checkId(activityId, "Activity ID");
+  const logs = await activity();
+  const log = await logs.findOne({ _id: new ObjectId(activityId) });
+  if (!log) throw "Error: activity not found";
+  return log;
+};
+
+// Get all activity logs for a given username
+const getLogsByUsername = async (username) => {
+  // need to add check here from user data functions to see if user exists
+  const logs = await activity();
+  return await logs.find({ username: username }).toArray();
+};
+
+const getAllLogs = async () => {
+  const logs = await activity();
+  const all = await logs.find({}).toArray();
+  return all;
+};
+
+// Edit an activity log
+const editLog = async (activityId, movieId, username, review, rating, date) => {
+  validation.checkProvided(activityId, movieId, username, review, rating, date);
+  activityId = validation.checkId(activityId, "Activity ID");
+  movieId = validation.checkMovieId(movieId);
+  username = validation.checkUsername(username);
+  review = validation.checkString(review, "Review");
+  rating = validation.checkRating(rating, "Rating");
+  date = validation.checkDate(date, "Date");
+  const activities = await activity();
+  const log = await getLogById(activityId);
+
+  const update = {
+    movieId: movieId,
+    review: review,
+    rating: rating,
+    date: date,
+  };
+  let hasChanges = false;
+  if (
+    update.movieId !== log.movieId ||
+    update.review !== log.review ||
+    update.rating !== log.rating ||
+    update.date !== log.date
+  )
+    hasChanges = true;
+  if (!hasChanges) throw "Error: no changes made";
+  const updatedLog = await activities.findOneAndReplace(
+    { _id: new ObjectId(activityId) },
+    update,
+    { returnDocument: "after" }
+  );
+  if (updatedLog.lastErrorObject.n === 0)
+    throw "Error: could not update activity";
+  //call updatetrening function in trending.js to update the trending once implemented
+  return updatedLog.value;
+};
+
+const deleteLog = async (activityId) => {
+  activityId = validation.checkId(activityId, "Activity ID");
+  const logs = await activity();
+  const deletedLog = await logs.findOneAndDelete({
+    _id: new ObjectId(activityId),
+  });
+  if (deletedLog.lastErrorObject.n === 0)
+    throw "Error: could not delete activity";
+  //call updatetrening function in trending.js to update the trending once implemented
+  return { activityId: activityId, deleted: true }; // will change the return object later based on requirements
+};
+
+export {
+  createLog,
+  getLogsByUsername,
+  getLogById,
+  getAllLogs,
+  editLog,
+  deleteLog,
+};
