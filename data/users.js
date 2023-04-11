efd2b0d45cc922be02cb707e7e84687622f6fd48
Parshotan Seenanan <parshsee@gmail.com>
2023-04-11 00:22:52 -0400

    Added update and delete functions

--- data/users.js
+++ data/users.js
@@ -11,17 +11,15 @@ const createUser = async (fName, lName, email, username, password) => {
 	fName = validation.checkString(fName, "First Name");
 	lName = validation.checkString(lName, "Last Name");
 	/* TODO:
-	= Use email validation once it's implemented
-	- Use username validation once it's implemented
 	- Improve validation/storing for password once we learn
 	  hashing and authentication
 	*/
-	email = validation.checkString(email, "Email");
-	username = validation.checkString(username, "Username");
+	email = validation.checkEmail(email);
+	username = validation.checkUsername(username);
 	password = validation.checkString(password, "Password");
 
 	const existingUsername = await getUserByUsername(username);
-	if (!existingUsername) throw `Error: User already exists with that username`;
+	if (existingUsername) throw `Error: User already exists with that username`;
 
 	let newUser = {
 		fName: fName,
@@ -40,7 +38,7 @@ const createUser = async (fName, lName, email, username, password) => {
 
 	if (!newUserInfo.insertedId || !newUserInfo.acknowledged) throw `Error: Could not add user`;
 
-	return await this.getUserById(newUserInfo.insertedId.toString());
+	return await getUserById(newUserInfo.insertedId.toString());
 
 };
 
@@ -72,7 +70,7 @@ const getUserById = async (userId) => {
 
 const getUserByUsername = async (username) => {
 	validation.checkProvided("Username", username);
-	username = validation.checkProvided(username, "Username");
+	username = validation.checkUsername(username);
 
 	const userCollection = await users();
 	const userInfo = userCollection.findOne({ username: this.username });
@@ -80,4 +78,81 @@ const getUserByUsername = async (username) => {
 	if (!userInfo) throw `Error: User not found`;
 
 	return userInfo;
+}
+
+const updateUser = async (userId, fName, lName, email, username, password) => {
+	validation.checkProvided("User Update Info", userId, fName, lName, email, username, password)
+	userId = validation.checkId(userId, "User ID");
+	fName = validation.checkString(fName, "First Name");
+	lName = validation.checkString(lName, "Last Name");
+	/* TODO:
+	- Improve validation/storing for password once we learn
+	  hashing and authentication
+	*/
+	email = validation.checkEmail(email);
+	username = validation.checkUsername(username);
+	password = validation.checkString(password, "Password");
+
+	const existingUser = await getUserById(id);
+
+	const updatedUserInfo = {
+		fName: fName,
+		lName: lName,
+		email: email,
+		username: username,
+		password: password
+	};
+
+	/* TODO:
+		- If user updates username, need a way to find every person that is following
+		the user and update their array.
+			--- Ignore this following is an array of userIds, can fetch their username regardless of changes to it
+	*/
+	checkUpdate(existingUser, updatedUserInfo);
+
+	const userCollection = await users();
+	const updatedUser = await userCollection.findOneAndUpdate(
+		{ _id: new ObjectId(userId) },
+		{ $set: updatedUserInfo },
+		{ returnDocument: "after" } 
+	);
+
+	if (updatedUser.lastErrorObject.n === 0) {
+		throw `Error: Could not update the user with id of ${userId}`;
+	};
+
+	updatedUser.value._id = updatedUser.value._id.toString();
+
+	return updatedUser.value;
+
+};
+
+const deleteUser = async (userId) => {
+	validation.checkProvided("User ID", userId);
+	id = validation.checkId(userId, "User ID");
+
+	const userCollection = await users();
+	const deletedUser = await userCollection.findOneAndDelete(
+		{ _id: new ObjectId(userId) }
+	);
+
+	if (deletedUser.lastErrorObject.n === 0) {
+		throw `Error: Could not delete the user with id of ${userId}`;
+	};
+
+	const returnObj = {
+		userId: deletedUser.value._id,
+		deleted: true
+	}
+
+	return returnObj;
+}
+
+const checkUpdate = (existingUser, updatedUserInfo) => {
+	if(existingUser.fName === updatedUserInfo.fName
+		&& existingUser.lName === updatedUserInfo.lName
+		&& existingUser.email === updatedUserInfo.email
+		&& existingUser.username === updatedUserInfo.email
+		&& existingUser.password === updatedUserInfo.password)
+		throw "Error: No changes found";
 }
