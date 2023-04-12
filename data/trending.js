fe68a6c3d6b4e2d682595b8d98c294e70390baba
atharvaa9 <APARTE@STEVENS.EDU>
2023-04-12 14:02:54 -0400

    Added createTrendingObject function

--- data/trending.js
+++ data/trending.js
@@ -1,3 +1,43 @@
 // create trending object for each new date
 // update trending object for each activity change (addition/deletion) if date already exists
 // delete activity id from array of trending object for each deleted activity
+
+import { trending } from "../config/mongoCollections.js";
+import { activity } from "../config/mongoCollections.js";
+import { ObjectId } from "mongodb";
+import * as validation from "../utils/validation.js";
+
+
+/*We are going to add only the id into activity.js as well The structure of the object there is going to be
+    trendingObject = {
+      date: 'string',
+      activityIds:[Array of acticity IDs]
+    }
+
+    Here we will be adding on what date what activityid was logged into our application
+  */
+    const createTendingObject = async (date,activityId) => {
+        date = validation.checkDate(date, "Date");
+        activityId = validation.checkId(activityId, "Activity ID");
+
+        const isTheGivenDatePresent = await trending.findOne({ date });
+
+        if(isTheGivenDatePresent){
+            await trending.updateOne(
+                { _id: isTheGivenDatePresent._id },
+                { $addToSet: { activityIds: activityId } }
+            )
+        }
+
+        else {
+            await trending.insertOne({ date, activityIds: [activityId] });
+        }
+        // let trendingObject = { 
+        //     date: date,
+        //     activityIds: activityId
+        // };
+    }    
+    const getActivityIdsForDate = async(date) => {
+        /* Implement your logic here to retrieve the list of activity IDs
+        for the given date from your data source */
+    }
