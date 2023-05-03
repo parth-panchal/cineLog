0efd2e2753d7f9e6e6e4a5bd2c894d30c2e2f967
Parshotan Seenanan <parshsee@gmail.com>
2023-05-02 20:24:45 -0400

    Worked on routes for homepage and search flow

--- routes/search.js
+++ routes/search.js
@@ -6,41 +6,85 @@ import { searchMovie } from "../utils/helper.js";
 
 const router = Router();
 
+// TODO: Implmement proper error page or error rendering
+// Possible TODO: Implement pagination for getting movies or users past the 20th one
+
 router
-    .route('/movies/:movieTitle')
-    .get(async (req, res) => {
-        let movieTitle = xss(req.params.movieTitle);
+    .route('/')
+    .post(async (req, res) => {
+        let searchTerm = xss(req.body.searchTerm);
+        let searchOption = xss(req.body.searchOption);
+        let redirectUrl = "/search";
 
         try {
-            movieTitle = validation.checkString(movieTitle, "Movie Title");
+            searchTerm = validation.checkString(searchTerm, "Search Term");
         } catch (error) {
             return res.status(400).json({error: error});
         }
 
         try {
-            const moviesInfo = await searchMovie(movieTitle);
-            res.render('searchResults', {results: moviesInfo});
+            searchOption = validation.checkString(searchOption, "Search Option");
+            if(searchOption !== "movies" && searchOption !== "users") throw `Error: Search Option value can only be 'movies' or 'users`;
+            
+            redirectUrl = redirectUrl + `/${searchOption}/${searchTerm}`;
         } catch (error) {
-            return res.status(500).json({error: error});
+            return res.status(400).json({error: error});
+        }
+
+        return res.redirect(redirectUrl);
+
+    });
+
+router
+    .route('/movies/:searchTerm')
+    .get(async (req, res) => {
+        let searchTerm = xss(req.params.searchTerm);
+        console.log(searchTerm);
+
+        try {
+            searchTerm = validation.checkString(searchTerm, "Movie Title");
+        } catch (error) {
+            return res.status(400).json({error: error});
+        }
+
+        try {
+            const moviesInfo = await searchMovie(searchTerm);
+            console.log(moviesInfo.results);
+            return res.render('searchResults', {
+                results: moviesInfo.results,
+                noResults: false,
+                isMovieSearch: true
+            });
+        } catch (error) {
+            res.render('searchResults', {
+                noResults: true
+            });
         }
     });
 
 router
-    .route('/users/:username')
+    .route('/users/:searchTerm')
     .get(async (req, res) => {
-        let username = xss(req.params.username);
+        let searchTerm = xss(req.params.searchTerm);
 
         try {
-            username = validation.checkString(username, "Username search query");
+            searchTerm = validation.checkString(searchTerm, "Username");
         } catch (error) {
             return res.status(400).json({error: error});
         }
 
         try {
-            const users = await userData.getUserByUsernamePartial(username);
-            res.render('searchResults', {results: users});
+            const users = await userData.getUserByUsernamePartial(searchTerm);
+            console.log(users);
+            return res.render('searchResults', {
+                results: users,
+                noResults: false,
+                isMovieSearch: false
+            });
         } catch (error) {
-            return res.status(500).json({error: error});
+            res.render('searchResults', {
+                noResults: true
+            });
         }
     });
 
