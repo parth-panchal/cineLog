64781420896711b575aa8096c3138f9d3e06c93c
Parth Panchal <parth.sdis@gmail.com>
2023-05-05 18:52:54 -0400

    changes

--- routes/profile.js
+++ routes/profile.js
@@ -155,7 +155,10 @@ router
         req.body.movieId,
         req.body.operation
       );
+      console.log(updatedWatchlistNumbersObject);
+      console.log("here");
       let updatedWatchlistNumbers = updatedWatchlistNumbersObject.watchlist;
+      console.log(updatedWatchlistNumbers);
       let updatedWatchlist = [];
       try {
         updatedWatchlist = await updatedWatchlistNumbers.reduce(
