61a6c4ec95bd50b6cc32fe8b30bac03552f9c2f6
atharvaa9 <APARTE@STEVENS.EDU>
2023-05-08 22:02:32 -0400

    partially completed client-side validations

--- routes/activity.js
+++ routes/activity.js
@@ -12,14 +12,7 @@ import xss from "xss";
 router
     .route('/:id')
     .get(async (req, res) => {
-        // if (!ObjectId.isValid(req.params.id)) {
-        //     res.status(400).json({ error: "Provided id is not valid" });
-        //     return;
-        // }
-        // console.log("inside route")
-        
         let activityId = xss(req.params.id); 
-        console.log(activityId)
 
         try {
             activityId = await validation.checkId(activityId)
@@ -30,13 +23,7 @@ router
         try {
             const activityInfo = await activityData.getLogById(activityId);
             const movieInfo = await getMovieInfo(activityInfo.movieId);
-
-
-            console.log(movieInfo)
-            // console.log("------------------------")
-            res.status(200).render('activityById', {title: "Activity", results : activityInfo, movieName : movieInfo.original_title});
-
-            
+            res.status(200).render('activityById', {title: "Activity", results : activityInfo, movieName : movieInfo.title, poster_path : movieInfo.poster_path });
           } catch (e) {
             res.status(404).render('error',{ error: "Activity not found with the provided id"});
             //render the error handlebar for all the catch
@@ -46,22 +33,20 @@ router
 
     .delete(async (req,res) => {
         let activityId = xss(req.params.id);
-        // console.log("check1")
+
         try {
-            // console.log("check2")
             activityId = await validation.checkId(activityId)
         } catch (error) {
-            // console.log("e1")
             return res.status(400).json({error: error});
         }
+
         try {
-            // console.log("check3")
             await activityData.getLogById(activityId);
         } catch (error) {
             return res.status(400).json({error: error});
         }
+
         try {
-            // console.log("check4")
             deletedActivity = await activityData.deleteLog(activityId)
             res.status(200).json({ message: 'Log deleted successfully', deletedActivity });
         } catch (error) {
@@ -70,33 +55,23 @@ router
 
     })
 
-
-router
-    .route('/:id/update')
     .patch(async (req, res) => {
         let activityInfo = req.body;
-        // console.log(activityInfo)
         let activityId = xss(req.params.id);
-        let userId = req.session.user.id;
-        let movieId = xss(req.body.movieId);
-        let review = activityInfo.review;
-        let rating = activityInfo.rating;
-        let date = activityInfo.date;
-        validation.checkProvided(movieId, userId, review, rating);
-        movieId = await validation.checkMovieId(movieId, "Movie ID");
-        userId = validation.checkId(userId, "User ID"); 
-        review = validation.checkString(review, "Review");
-        rating = validation.checkRating(rating, "Rating");
-        date = validation.checkDate(date, "Date");
+        let userId = xss(req.session.user._id);
+        let movieId = xss(activityInfo.movieId);        
+        let review = xss(activityInfo.review);; 
+        let rating = xss(activityInfo.rating);
+        let date = xss(activityInfo.date);
 
         //here user id will remain the same
         //the only things that the user can change are review rating movie id and date
-        try{
-            const updatedActivity = await activityData.editLog(activityId, review, rating, date);
-            res.status(200).json({ message: 'Log updated successfully', updatedActivity });
 
+        try{
+            const updatedActivity = await activityData.editLog(activityId, movieId, userId, review, rating, date);
+            res.status(200).json({ message: 'Log updated successfully', updatedActivity });
         } catch (error) {
-            return res.status(400).render({error: "There was some problem in updating the log"});
+            return res.status(400).render('error', { error: error.toString() });
         }
     })
 
